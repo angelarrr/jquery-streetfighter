7e7758a2d97bb886065368a5579261868c197604
angelarrr <kwon.angela@gmail.com>
2015-05-28 13:12:14 -0400

    Animate hadouken and add sound

--- js/app.js
+++ js/app.js
@@ -7,15 +7,27 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
-	.mousedown(function() {
-		//play Hadouken sound
+	.mousedown(function(){
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//animate hadouken to right of the screen
+		$('.hadouken').finish().show().animate(
+			{'left': '1133px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '633px');
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	})
+	});
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
