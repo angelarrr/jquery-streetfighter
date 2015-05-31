4b9a56b37ac2ddfdcc9059146af9054a29e6b601
angelarrr <kwon.angela@gmail.com>
2015-05-31 19:22:00 -0400

    Add cool guy pose on keydown

--- js/app.js
+++ js/app.js
@@ -26,6 +26,20 @@ $(document).ready(function() {
 	});
 });
 
+$(document).keydown(function(e) {
+	if(event.keyCode == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-cool').show();
+	}
+})
+.keyup(function(e) {
+	if(event.keyCode == 88) {
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	}
+});
+
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
